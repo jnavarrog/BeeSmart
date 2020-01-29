abf2c5aa0c35da48e006f7b9762cb2e257eb750d
jnavarrog <j@mail.com>
2020-01-28 21:15:22 -0300

    Se agrega el proyecto principal node y el recurso para la temperatura

---
+++ bee_smart/node/resources/res-temperature.c
@@ -0,0 +1,162 @@
+/*
+ * Copyright (c) 2013, Institute for Pervasive Computing, ETH Zurich
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ */
+
+/**
+ * \file
+ *      Example resource
+ * \author
+ *      Matthias Kovatsch <kovatsch@inf.ethz.ch>
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "coap-engine.h"
+#include <DS18B20_SENSOR.h>
+#include <math.h>
+#include <stdio.h>
+
+//Defino los handler para cada metodo
+static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void res_post_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void res_put_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void res_delete_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+
+//Defino el recurso
+RESOURCE(res_temperature,
+         "title=\"temperature",
+         res_get_handler,
+	 res_post_handler,
+	 res_put_handler,
+         res_delete_handler);
+
+//GET
+static void
+res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+
+  const char *len = NULL;
+
+  /* Some data that has the length up to REST_MAX_CHUNK_SIZE. For more, see the chunk resource. */
+
+  //Respuesta del GET
+  //----------------------------------------------------------------------------------------------/
+  char message[62];
+  sprintf(message, "Temp: ");
+  int ds18b20_amount_int_res = DS18B20_AMOUNT_INT;
+  
+  for(int i = 0; i < ds18b20_amount_int_res; i++) {
+      ds18b20.configure(DS18B20_CONFIGURATION_INDEX, i);
+      ds18b20.configure(DS18B20_CONFIGURATION_READ, 0);
+
+      int address_low = ds18b20.value(DS18B20_VALUE_ADDRESS_LOW);
+      int address_high = ds18b20.value(DS18B20_VALUE_ADDRESS_HIGH);
+      int integer = ds18b20.value(DS18B20_VALUE_TEMPERATURE_INTEGER);
+      int decimal = ds18b20.value(DS18B20_VALUE_TEMPERATURE_DECIMAL);
+
+      printf("Address %x%x: %d,%d\n", address_high, address_low, integer, decimal);
+     
+     sprintf(message + strlen(message), "%d.%d-",integer, decimal);
+
+		  
+            
+  }//end for
+  printf("MESSAGE LEN: %d\n",strlen(message));
+
+  //----------------------------------------------------------------------------------------------/
+
+  int length = strlen(message);
+
+  /* The query string can be retrieved by rest_get_query() or parsed for its key-value pairs. */
+  if(coap_get_query_variable(request, "len", &len)) {
+    length = atoi(len);
+    if(length < 0) {
+      length = 0;
+    }
+    if(length > REST_MAX_CHUNK_SIZE) {
+      length = REST_MAX_CHUNK_SIZE;
+    }
+    memcpy(buffer, message, length);
+  } else {
+    memcpy(buffer, message, length);
+  }
+
+  coap_set_header_content_format(response, TEXT_PLAIN); /* text/plain is the default, hence this option could be omitted. */
+  coap_set_header_etag(response, (uint8_t *)&length, 1);
+  coap_set_payload(response, buffer, length);
+
+  printf("[LOG: User] GET successful\n");
+
+}// end get
+
+//POST
+static void
+res_post_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+
+	const uint8_t * payload;
+
+	/*int len = */ coap_get_payload(request, &payload);
+
+	//Datos recibidos en el post (estan en la variable payload)
+	printf("DATA: %s\n", payload);
+
+	printf("[LOG: User] POST successful\n");
+
+
+}//end post
+
+//PUT
+static void
+res_put_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+
+	const uint8_t * payload;
+
+	/*int len = */ coap_get_payload(request, &payload);
+
+	//Datos recibidos en el put (estan en la variable payload)
+	printf("DATA: %s\n", payload);
+
+	printf("[LOG: User] PUT successful\n");
+
+
+}//end put
+
+//DELETE
+static void
+res_delete_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+
+  printf("[LOG: User] DELETE successful\n");
+
+}// end delete
+
