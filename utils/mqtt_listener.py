9014232f357c689c0e80e240e7852297c10c90ef
jnavarrog <j@mail.com>
2020-02-07 16:19:58 -0300

    Se continua trabajando con el programa principal

--- utils/mqtt_listener.py
+++ utils/mqtt_listener.py
@@ -17,8 +17,11 @@ def main():
 	global user
 	global passw
 	global mutex
+	global time_upd_nlist
+	global coap_max_wtime
 	f=open("BEESMART.conf","r")
 	line=f.readlines()
+	#load BEESMART.conf configuration parameters
 	for x in line:
 		if "M_BROKER" in x:
 			broker=x.split("=")[1]
@@ -32,6 +35,12 @@ def main():
 		elif "M_PASS" in x:
 			passw=x.split("=")[1]
 			passw=passw.rstrip()
+		elif "T_UPDATE_NODELIST" in x:
+			time_upd_nlist=x.split("=")[1]
+			time_upd_nlist=time_upd_nlist.rstrip()
+		elif "COAP_MAX_WAIT_TIME" in x:
+			coap_max_wtime=x.split("=")[1]
+			coap_max_wtime=coap_max_wtime.rstrip()
 	f.close()
 	mutex = threading.Lock()
 	mosquitto_tr = threading.Thread(target=mosquitto_init, name='mosquitto_tr')
@@ -49,31 +58,23 @@ def get_nodelist():
 		if "IPv6_BR_GA" in x:
 			ip_br=x.split("=")[1]
 			ip_br=ip_br.rstrip()
+	f.close()
 	while 1:
-		time.sleep(5)
+		print("whileeee")
+		#if os.path.exists("NODELIST.dat"):
+			#os.remove("NODELIST.dat")
 		webURL=urllib2.urlopen('http://['+ip_br+']/')
+		f=open("NODELIST.dat","a")
 		for y in webURL.readlines():
 			if "node_ipv6" in y:
 				nodeip=y.rstrip()
-   				print(nodeip)
+				print(nodeip)
+				f.write(nodeip+"\n")
+   		f.close
+   		time.sleep(float(time_upd_nlist))
    				
-def coap_handler(coap_address, method, resource):
-	mutex.acquire()
-	#usando coap-client de linux
-	out = subprocess.Popen(['coap-client', '-v', '0','-B','3','-m',method,'coap://['+coap_address+']/'+resource], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	stdout,stderr = out.communicate()
-	print(stdout)
-	#usando la api de python
-	#coapc = HelperClient(server=(coap_server, 5683))
-	#cresponse = coapc.get(resource)
-	#print(cresponse.payload)
-	#coapc.stop()
-	mutex.release()
 	
 def run_tunslip():
-	#os.system("rm -rf IP_BR_DATA.dat")
-	#time.sleep(0.2)
-	#os.system("./tunslip6 -s /dev/ttyACM0 fd00::1/64")
 	out = subprocess.Popen(['./tunslip6', '-s', '/dev/ttyACM0','fd00::1/64'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	stdout,stderr = out.communicate()
 	print(stdout)
@@ -92,15 +93,41 @@ def run_tunslip():
 	
 #def on_publish(mqttc, obj, mid):
     	#print("-->mid: " + str(mid))
+    	
+def coap_handler(coap_address, method, resource, data):
+	mutex.acquire()
+	#usando coap-client de linux
+	
+	if method is 'get':
+		out = subprocess.Popen(['coap-client', '-v', '0','-B',coap_max_wtime,'-m',method,'coap://['+coap_address+']/'+resource], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		
+	if method is 'post' or method is 'put':
+		out = subprocess.Popen(['coap-client', '-v', '0','-B','10',coap_max_wtime,method,'-e',data,'coap://['+coap_address+']/'+resource], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		
+	stdout,stderr = out.communicate()
+	print(stdout)
+	client.publish("data", stdout)
+	mutex.release()
+	#usando la api de python
+	#coapc = HelperClient(server=(coap_server, 5683))
+	#cresponse = coapc.get(resource)
+	#print(cresponse.payload)
+	#coapc.stop()
+
  
 def on_message(client, userdata, message):
-	print("RECIBI ALGO")
-	print("message received " ,str(message.payload.decode("utf-8")))
-	print("message topic=",message.topic)
-	print("message qos=",message.qos)
-	print("message retain flag=",message.retain)
-	coap_handler("fd00::212:4b00:1375:df84", "get", "sensors/temperature/all")
-	
+#mosquitto_pub -h beesmart.cure.edu.uy -p 2000 -t "send/METHOD" -u pfc -P pfc -m "ADDRESS%TOPIC%DATA"
+	#print("message received " ,str(message.payload.decode("utf-8")))
+	#print("message topic=",message.topic)
+	#print("message qos=",message.qos)
+	#print("message retain flag=",message.retain)
+	if "get" in message.topic:
+		coap_handler(message.payload.split("%")[0], "get", message.payload.split("%")[1], "")
+	elif "post" in message.topic:
+		coap_handler(message.payload.split("%")[0], "post", message.payload.split("%")[1],message.payload.split("%")[2])
+	elif "put" in message.topic:
+		coap_handler(message.payload.split("%")[0], "put", message.payload.split("%")[1],message.payload.split("%")[2])
+		
 def mosquitto_init():
 	client.username_pw_set(user, passw)
 	#client.on_subscribe = on_subscribe
@@ -110,7 +137,7 @@ def mosquitto_init():
 	#client.on_connect = on_connect
 	#client.on_disconnect = on_disconnect
 	client.connect(broker, port)
-	client.subscribe("prueba", qos=1)	
+	client.subscribe("send/#", qos=1)	
 	client.loop_forever()
 
 	
