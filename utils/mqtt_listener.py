b305a38859cbb68f843f775823b516102a14ccd3
jnavarrog <j@mail.com>
2020-02-09 19:48:56 -0300

    Se agrega la funcion para obtener los datos automaticamente

--- utils/mqtt_listener.py
+++ utils/mqtt_listener.py
@@ -7,7 +7,8 @@ import threading
 import time
 from coapthon.client.helperclient import HelperClient
 import urllib2
-
+import datetime
+import re
 global client
 client = paho.Client()
 
@@ -17,8 +18,13 @@ def main():
 	global user
 	global passw
 	global mutex
+	global accessfile
 	global time_upd_nlist
 	global coap_max_wtime
+	global time_between_reqall
+	global data_resources
+	global tun_usbport
+	global ipv6_tunnel
 	f=open("BEESMART.conf","r")
 	line=f.readlines()
 	#load BEESMART.conf configuration parameters
@@ -38,11 +44,28 @@ def main():
 		elif "T_UPDATE_NODELIST" in x:
 			time_upd_nlist=x.split("=")[1]
 			time_upd_nlist=time_upd_nlist.rstrip()
-		elif "COAP_MAX_WAIT_TIME" in x:
+		elif "T_COAP_MAX_WAIT_TIME" in x:
 			coap_max_wtime=x.split("=")[1]
 			coap_max_wtime=coap_max_wtime.rstrip()
+		elif "T_REQUEST_ALL_DATA" in x:
+			time_between_reqall=x.split("=")[1]
+			time_between_reqall=time_between_reqall.rstrip()
+		elif "L_COAP_DATA_RESOURCES" in x:
+			data_resources=x.split("=")[1]
+			data_resources=data_resources.rstrip()
+		elif "D_USB_PORT_TUN" in x:
+			tun_usbport=x.split("=")[1]
+			tun_usbport=tun_usbport.rstrip()
+		elif "D_IPV6_TUNNEL" in x:
+			ipv6_tunnel=x.split("=")[1]
+			ipv6_tunnel=ipv6_tunnel.rstrip()	
 	f.close()
+	
+	f=open("NODELIST.dat","w")
+	f.close()
+		
 	mutex = threading.Lock()
+	accessfile = threading.Lock()
 	mosquitto_tr = threading.Thread(target=mosquitto_init, name='mosquitto_tr')
 	mosquitto_tr.start()
 	tunslip6_tr = threading.Thread(target=run_tunslip)
@@ -50,8 +73,11 @@ def main():
 	time.sleep(5)
 	get_nodelist_tr = threading.Thread(target=get_nodelist)
 	get_nodelist_tr.start()
+	get_alldata_tr = threading.Thread(target=get_alldata)
+	get_alldata_tr.start()
 	
 def get_nodelist():
+	nodeip=""
 	f=open("IP_BR_DATA.dat","r")
 	line=f.readlines()
 	for x in line:
@@ -60,22 +86,41 @@ def get_nodelist():
 			ip_br=ip_br.rstrip()
 	f.close()
 	while 1:
-		print("whileeee")
-		#if os.path.exists("NODELIST.dat"):
-			#os.remove("NODELIST.dat")
+		print("while node list")
 		webURL=urllib2.urlopen('http://['+ip_br+']/')
-		f=open("NODELIST.dat","a")
 		for y in webURL.readlines():
 			if "node_ipv6" in y:
-				nodeip=y.rstrip()
-				print(nodeip)
-				f.write(nodeip+"\n")
+				nodeip=nodeip+y.rstrip()+"\n"
+
+		accessfile.acquire()
+		f=open("NODELIST.dat","w")
+		f.write(nodeip)
    		f.close
+   		accessfile.release()
+   		nodeip=""
+   		#print(nodeip)
    		time.sleep(float(time_upd_nlist))
    				
-	
+def get_alldata():
+	while 1:
+		print("while get all")
+		accessfile.acquire()
+		f=open("NODELIST.dat","r")
+		line=f.readlines()
+		for x in line:
+			if "node_ipv6" in x:
+				ipnode=x.split("-")[0].split("]")[0].split("[")[1].rstrip()
+				print(ipnode)
+				for res in data_resources.split(","):
+					print(res)
+					coap_handler(ipnode, "get", res, "")
+					
+   		f.close
+   		accessfile.release()
+		time.sleep(float(time_between_reqall))
+
 def run_tunslip():
-	out = subprocess.Popen(['./tunslip6', '-s', '/dev/ttyACM0','fd00::1/64'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	out = subprocess.Popen(['./tunslip6', '-s',tun_usbport,ipv6_tunnel], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	stdout,stderr = out.communicate()
 	print(stdout)
 	
@@ -97,23 +142,22 @@ def run_tunslip():
 def coap_handler(coap_address, method, resource, data):
 	mutex.acquire()
 	#usando coap-client de linux
-	
+	ts = time.time()
+	timestamp = datetime.datetime.fromtimestamp(ts).strftime("%d-%m-%Y (%H:%M:%S.%f)")	
 	if method is 'get':
-		out = subprocess.Popen(['coap-client', '-v', '0','-B',coap_max_wtime,'-m',method,'coap://['+coap_address+']/'+resource], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-		
+		out = subprocess.Popen(['coap-client', '-v', '0','-B',coap_max_wtime,'-m',method,'coap://['+coap_address+']/'+resource], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)		
 	if method is 'post' or method is 'put':
-		out = subprocess.Popen(['coap-client', '-v', '0','-B','10',coap_max_wtime,method,'-e',data,'coap://['+coap_address+']/'+resource], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-		
+		out = subprocess.Popen(['coap-client', '-v', '0','-B',coap_max_wtime,'-m',method,'-e',data,'coap://['+coap_address+']/'+resource], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)		
 	stdout,stderr = out.communicate()
-	print(stdout)
-	client.publish("data", stdout)
+	datatosend=re.sub('v:1 t:CON.*]\s','',stdout)
+	print(datatosend.rstrip())
+	client.publish("data", "["+timestamp+"]"+"["+coap_address+"]"+"["+resource+"]"+"{"+datatosend.rstrip()+"}")
 	mutex.release()
 	#usando la api de python
 	#coapc = HelperClient(server=(coap_server, 5683))
 	#cresponse = coapc.get(resource)
 	#print(cresponse.payload)
 	#coapc.stop()
-
  
 def on_message(client, userdata, message):
 #mosquitto_pub -h beesmart.cure.edu.uy -p 2000 -t "send/METHOD" -u pfc -P pfc -m "ADDRESS%TOPIC%DATA"
