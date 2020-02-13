8a1700670c79cc21e9273a49e186d360075f95bf
Juan Navarro <jangng5@gmail.com>
2020-02-13 20:26:43 -0300

    Se agrega un readme y el sistema de logs

--- utils/beesmart.py
+++ utils/beesmart.py
@@ -20,6 +20,7 @@ global accessfile
 accessfile = threading.Lock()
 
 def main():
+	LOG("Inicia beesmart.py")
 	global broker
 	global port
 	global user
@@ -32,8 +33,8 @@ def main():
 	global data_resources
 	global tun_usbport
 	global ipv6_tunnel
-	global idbeehive
-	global descbeehive
+	global idapiario
+	global descapiario
 	f=open("BEESMART.conf","r")
 	line=f.readlines()
 	#load BEESMART.conf configuration parameters
@@ -68,26 +69,32 @@ def main():
 		elif "D_IPV6_TUNNEL" in x:
 			ipv6_tunnel=x.split("=")[1]
 			ipv6_tunnel=ipv6_tunnel.rstrip()
-		elif "D_ID_BEEHIVE" in x:
-			idbeehive=x.split("=")[1]
-			idbeehive=idbeehive.rstrip()
-		elif "D_BEEHIVE_DESC" in x:
-			descbeehive=x.split("=")[1]
-			descbeehive=descbeehive.rstrip()	
+		elif "D_ID_APIARIO" in x:
+			idapiario=x.split("=")[1]
+			idapiario=idapiario.rstrip()
+		elif "D_APIARIO_DESC" in x:
+			descapiario=x.split("=")[1]
+			descapiario=descapiario.rstrip()	
+	LOG("Configuración cargada")
 	f.close()
 	
 	f=open("NODELIST.dat","w")
 	f.close()
 	
+	LOG("Se lanza thread mosquitto_tr")
 	mosquitto_tr = threading.Thread(target=mosquitto_init, name='mosquitto_tr')
 	mosquitto_tr.start()
+	LOG("Se lanza thread tunslip6")
 	tunslip6_tr = threading.Thread(target=run_tunslip)
 	tunslip6_tr.start()
 	time.sleep(5)
+	LOG("Se lanza thread get_nodelist_tr")
 	get_nodelist_tr = threading.Thread(target=get_nodelist)
 	get_nodelist_tr.start()
+	LOG("Se lanza thread get_alldata_tr")
 	get_alldata_tr = threading.Thread(target=get_alldata)
 	get_alldata_tr.start()
+	LOG("Fin main")
 	
 def get_nodelist():
 	nodeip=""
@@ -99,7 +106,6 @@ def get_nodelist():
 			ip_br=ip_br.rstrip()
 	f.close()
 	while 1:
-		print("while node list")
 		webURL=urllib2.urlopen('http://['+ip_br+']/')
 		for y in webURL.readlines():
 			if "node_ipv6" in y:
@@ -111,12 +117,12 @@ def get_nodelist():
    		accessfile.release()
    		nodeip=""
    		#print(nodeip)
+   		LOG("Se actualiza la lista de nodos")
    		time.sleep(float(time_upd_nlist))
    				
 def get_alldata():
 	nodearray=[]
 	while 1:
-		print("while get all")
 		accessfile.acquire()
 		f=open("NODELIST.dat","r")
 		line=f.readlines()
@@ -131,12 +137,13 @@ def get_alldata():
    			for res in data_resources.split(","):
    				coap_handler(ip, "get", res, "")
    		del nodearray[:]
+   		LOG("Se recolectaron y publicar automaticamente todos los datos")
 		time.sleep(float(time_between_reqall))
 
 def run_tunslip():
 	out = subprocess.Popen(['./tunslip6', '-s',tun_usbport,ipv6_tunnel], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	stdout,stderr = out.communicate()
-	print(stdout)
+	#print(stdout)
 	
 #def on_connect(client, userdata, flags, rc):
 	#print("Connected With Result Code: {}".format(rc))
@@ -154,34 +161,27 @@ def run_tunslip():
     	#print("-->mid: " + str(mid))
     	
 def coap_handler(coap_address, method, resource, data):
-	#print("000000000000",threading.currentThread().getName())
 	mutex.acquire()
-	#print("111111111111",threading.currentThread().getName())
 	#usando coap-client de linux
 	ts = time.time()
-	timestamp = datetime.datetime.fromtimestamp(ts).strftime("%d-%m-%Y (%H:%M:%S.%f)")	
+	timestamp = datetime.datetime.fromtimestamp(ts).strftime("%d-%m-%Y (%H:%M:%S.%f)")
+	LOG("INICIO: coap-client "+coap_address+" "+method+" "+resource+" "+data)	
 	if method is 'get':
 		out = subprocess.Popen(['coap-client', '-v', '0','-B',coap_max_wtime,'-m',method,'coap://['+coap_address+']/'+resource], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-		#print("2222222222222222",threading.currentThread().getName())		
 	if method is 'post' or method is 'put':
 		out = subprocess.Popen(['coap-client', '-v', '0','-B',coap_max_wtime,'-m',method,'-e',data,'coap://['+coap_address+']/'+resource], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)		
 	stdout,stderr = out.communicate()
+	LOG("FIN: coap-client "+coap_address+" "+method+" "+resource+" "+data)	
 	datatosend=re.sub('v:1 t:CON.*]\s','',stdout)
 	print(datatosend.rstrip())
-	#print("333333333333",threading.currentThread().getName())
-	pub.publish(idbeehive+"/data", "["+timestamp+"]"+"["+coap_address+"]"+"["+resource+"]"+"{"+datatosend.rstrip()+"}")
-	#print("444444444444",threading.currentThread().getName())
-	#usando la api de python
-	#coapc = HelperClient(server=(coap_server, 5683))
-	#cresponse = coapc.get(resource)
-	#print(cresponse.payload)
-	#coapc.stop()
+	LOG("Publicando datos")
+	pub.publish(idapiario+"/data", "["+timestamp+"]"+"["+coap_address+"]"+"["+resource+"]"+"{"+datatosend.rstrip()+"}")
+	LOG("Datos publicados")
 	mutex.release()
-	#print("5555555555",threading.currentThread().getName())
  	
 def on_message(client, userdata, message):
-#mosquitto_pub -h beesmart.cure.edu.uy -p 2000 -t "send/METHOD" -u pfc -P pfc -m "ADDRESS%TOPIC%DATA"
-	print("message received " ,str(message.payload.decode("utf-8")))
+	LOG("on message")
+	#print("message received " ,str(message.payload.decode("utf-8")))
 	#print("message topic=",message.topic)
 	#print("message qos=",message.qos)
 	#print("message retain flag=",message.retain)
@@ -194,37 +194,38 @@ def on_message(client, userdata, message):
 	elif "rpicommands" in message.topic:
 		if "nodelist" in message.payload:
 			mutex.acquire()
-			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idbeehive+"/rpicommands -u "+ user+ " -P "+ passw + " -f NODELIST.dat")
+			LOG("Se ejecutó el comando nodelist")
+			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idapiario+"/rpicommands -u "+ user+ " -P "+ passw + " -f NODELIST.dat")
 			mutex.release()
 		elif "configfile" in message.payload:
 			mutex.acquire()
-			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idbeehive+"/rpicommands -u "+ user+ " -P "+ passw + " -f BEESMART.conf")
+			LOG("Se ejecutó el comando configfile")
+			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idapiario+"/rpicommands -u "+ user+ " -P "+ passw + " -f BEESMART.conf")
 			mutex.release()
 		elif "iprpi" in message.payload:
 			mutex.acquire()
+			LOG("Se ejecutó el comando iprpi")
 			os.system("ifconfig > iptemp.temp")
 			time.sleep(0.2)
-			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idbeehive+"/rpicommands -u "+ user+ " -P "+ passw + " -f iptemp.temp")
+			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idapiario+"/rpicommands -u "+ user+ " -P "+ passw + " -f iptemp.temp")
 			time.sleep(0.2)
 			os.system("rm -rf iptemp.temp")
 			mutex.release()
 		elif "description" in message.payload:
 			mutex.acquire()
-			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idbeehive+"/rpicommands -u "+ user+ " -P "+ passw + " -m "+descbeehive)
-			mutex.release()
-		elif "rebootnodes" in message.payload:
-			mutex.acquire()
-			os.system("echo rebootnodes")
+			LOG("Se ejecutó el comando description")
+			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idapiario+"/rpicommands -u "+ user+ " -P "+ passw + " -m "+descapiario)
 			mutex.release()
 		elif "rebootrpi" in message.payload:
 			mutex.acquire()
+			LOG("Se ejecutó el comando rebootrpi")
 			os.system("reboot now")
 			mutex.release()
-		elif "rebootall" in message.payload:
+		else:
 			mutex.acquire()
-			os.system("echo rebootall")
+			LOG("Se ejecutó un comando invalido")
+			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idapiario+"/rpicommands -u "+ user+ " -P "+ passw + " -m comando invalido")
 			mutex.release()
-		
 			
 	
 def mosquitto_init():
@@ -238,8 +239,14 @@ def mosquitto_init():
 	#client.on_disconnect = on_disconnect
 	sub.connect(broker, port)
 	pub.connect(broker, port)
-	sub.subscribe(idbeehive+"/send/#", qos=1)	
+	sub.subscribe(idapiario+"/send/#", qos=1)
 	sub.loop_forever()
+	
+def LOG(msg):
+	print(msg)
+	ts = time.time()
+	timestamp = datetime.datetime.fromtimestamp(ts).strftime("%d-%m-%Y (%H:%M:%S.%f)")
+	os.system("echo '["+timestamp+"] "+msg+"' >> BEESMART.log")
 
 	
 if __name__== "__main__":
---
+++ utils/readme.md
@@ -0,0 +1,136 @@
+#BEESMART
+
+El programa princiapl beesmart.py se encarga de:
+-Crear el tunel entre la RPI y el BR
+-Detectar nodos automaticamente y agregarlo a la lista de nodos
+-Recolectar automaticamente los datos de los sensores y publicarlos
+-Recibir pedidos puntuales o comandos, procesarlos y responder
+-Registrar en un log todos los sucesos de importancia
+
+##REQUISITOS
+
+-Based Debian Linux (v9 o superior)
+-Python 2.7.17 (`apt-get install python2.7`)
+-mosquitto-client (`apt-get install mosquitto-clients`)
+-PAHO MQTT Python Client (`pip install paho-mqtt`)
+-ifconfig (`apt-get install net-tools`)
+-CoAP Client (`apt-get install libcoap-1-0`)
+-El programa debe ser ejecutado como root
+-El archivo de BEESMART.conf y tunslip6 deben estar en la misma carpeta
+
+##MQTT Topics
+
+###IDAPIARIO/data
+Todos los datos recolectados son publicados en este topic.
+
+###IDAPIRARIO/rpicommands
+Es donde se publican las salidas de los comandos ejecutados en la RPI (ver comunicación)
+
+###IDAPIARIO/send/#
+El programa principal se suscribe a este topic para recibir instrucciones desde el exterior. Ver la sección "Comunicación" por mas detalles.
+
+##CoAP Resources
+
+La siguiente lista de recursos se encuentra disponible en cada nodo
+
+###test
+-GET: Devuelte la dirección MAC del nodo
+-POST: No hace nada
+-PUT: No hace nada
+-DELETE: No hace nada
+
+###sensors/temperature/all
+-GET: Devuelte la dirección de cada sensor de temperatura y su valor de temperatura en ese momento
+-POST: No hace nada
+-PUT: No hace nada
+-DELETE: No hace nada
+
+###sensors/temperature/temperature
+-GET: Devuelte el valor de temperatura en ese momento de cada sensor de temperatura
+-POST: No hace nada
+-PUT: No hace nada
+-DELETE: No hace nada
+
+###commands/reboot
+-GET: Reinicia el nodo
+-POST: No hace nada
+-PUT: No hace nada
+-DELETE: No hace nada
+
+###sensors/weight
+-GET: Devuelte el peso de la colmena
+-POST: No hace nada
+-PUT: No hace nada
+-DELETE: No hace nada
+
+###actuators/servo
+-GET: Devuelte la posición actual de la puerta (1 cerrada, 0 abierta)
+-POST: Si se le envía un 1 se le indica que cierre la puerta, si se le envía un 0 se le indica que abra la puerta
+-PUT: Si se le envía un 1 se le indica que cierre la puerta, si se le envía un 0 se le indica que abra la puerta
+-DELETE: No hace nada
+
+##COMUNICACIÓN
+
+Cada vez que el apiario deba enviar datos (tanto de forma automática como respuesta a consultas puntuales) lo hará en el topic IDCOLMENA/data. A todo lo que es dato se le adjunta un timestap, el id del nodo que lo produjo, que tipo de dato es y el dato en si mismo para facilitar el procesamiento. La respuesta a la ejecución de "comandos" no incluye esta cabecera y se publica en IDAPIARIO/rpicommands
+
+```
+Formato datos: 
+[TIMESTAMP][IPV6NODE][COAPRESOURCE]{DATOS}
+```
+
+Para obtener datos de forma manual o enviarle comandos a la RPI o los nodos se utiliza el topic IDCOLMENA/send/# en combinación con el mensaje:
+
+-GET
+Para hacer un get (pedir un dato) a un nodo en particular se debe publicar en el topic `IDCOLMENA/send/get` el mensaje `NODEIPV6%RESOURCE%`
+
+-POST
+Para hacer un post y enviarle `DATA` a un nodo se debe publicar en el topic `IDCOLMENA/send/post` el mensaje `NODEIPV6%RESOURCE%DATA`
+
+-PUT
+Para hacer un post y enviarle `DATA` a un nodo se debe publicar en el topic `IDCOLMENA/send/put` el mensaje `NODEIPV6%RESOURCE%DATA`
+
+Depende el `RESOURCE` que se utilice es el dato que se obtendrá (ver CoAP Resources).
+
+-Comandos
+Para enviarle comando a la RPI se debe publicar en el topic `IDCOLMENA/send/rpicommands` el mensaje `COMMAND`
+
+-Comandos disponibles
+nodelist: devuelve la lista de nodos
+configfile: devuelve los valores del archivo BEESMART.conf
+iprpi: devuelve la salida del comando ifconfig ejecutado en la RPI
+description: devuelve la descripción del apiario
+rebootrpi: reinicia la RPI
+
+##BEESMART.conF
+En este archivo se almacena la configuración principal del programa beesmart.py
+
+*D_ID_BEEHIVE*: ID que identifica al apiario (a cada RPI)
+*D_BEEHIVE_DESC*: Descripción del apiario (ubicacion, nombre, etc)
+*M_BROKER*: IP o URL del broker mqtt
+*M_PORT*: Puerto del broker mqtt
+*M_USE*: Usuario del broker mqtt
+*M_PASS*: Contraseña del broker mqtt
+*T_UPDATE_NODELIST*: Cada cuanto tiempo (en segundos) se actualiza la lista de nodos
+*T_REQUEST_ALL_DATA*: Cada cuanto tiempo (en segundos) se recolecta y envía toda la información 
+*T_COAP_MAX_WAIT_TIME*: Cuanto tiempo espera como máximo CoAP la respuesta de un nodo (en segundos)
+*L_COAP_DATA_RESOURCES*: Lista de recursos CoAP que se consultarán automaticamente cada T_REQUEST_ALL_DATA segundos
+*D_USB_PORT_TUN*: Ubicación en /dev del puerto USB al cual se conecta el BR
+*D_IPV6_TUNNEL*: IPv6 que tomará la RPI (todos los nodos usarán el mismo prefijo)
+
+###Ejemplo
+
+```
+//BEESMART
+D_ID_APIARIO=colmena1
+D_APIARIO_DESC="colmena ubicada en algun lugar"
+M_BROKER=beesmart.cure.edu.uy
+M_PORT=2000
+M_USER=pfc
+M_PASS=pfc
+T_UPDATE_NODELIST=300
+T_REQUEST_ALL_DATA=900
+T_COAP_MAX_WAIT_TIME=5
+L_COAP_DATA_RESOURCES=test,sensors/temperature/all,sensors/weight,actuators/servo,sensors/temperature/temperature
+D_USB_PORT_TUN=/dev/ttyACM0
+D_IPV6_TUNNEL=fd00::1/64
+```
