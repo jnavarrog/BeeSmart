49b95eca6fcff987186c71c9ac762c5bfd18cc8a
Juan Navarro <jangng5@gmail.com>
2020-02-12 21:07:50 -0300

    Se agregan varios comandos para obtener información de la rpi

--- utils/mqtt_listener.py
+++ utils/mqtt_listener.py
@@ -9,8 +9,15 @@ from coapthon.client.helperclient import HelperClient
 import urllib2
 import datetime
 import re
-global client
-client = paho.Client()
+
+global sub
+global pub
+sub = paho.Client()
+pub = paho.Client()
+global mutex
+mutex = threading.Lock()
+global accessfile
+accessfile = threading.Lock()
 
 def main():
 	global broker
@@ -25,6 +32,8 @@ def main():
 	global data_resources
 	global tun_usbport
 	global ipv6_tunnel
+	global idbeehive
+	global descbeehive
 	f=open("BEESMART.conf","r")
 	line=f.readlines()
 	#load BEESMART.conf configuration parameters
@@ -58,14 +67,18 @@ def main():
 			tun_usbport=tun_usbport.rstrip()
 		elif "D_IPV6_TUNNEL" in x:
 			ipv6_tunnel=x.split("=")[1]
-			ipv6_tunnel=ipv6_tunnel.rstrip()	
+			ipv6_tunnel=ipv6_tunnel.rstrip()
+		elif "D_ID_BEEHIVE" in x:
+			idbeehive=x.split("=")[1]
+			idbeehive=idbeehive.rstrip()
+		elif "D_BEEHIVE_DESC" in x:
+			descbeehive=x.split("=")[1]
+			descbeehive=descbeehive.rstrip()	
 	f.close()
 	
 	f=open("NODELIST.dat","w")
 	f.close()
-		
-	mutex = threading.Lock()
-	accessfile = threading.Lock()
+	
 	mosquitto_tr = threading.Thread(target=mosquitto_init, name='mosquitto_tr')
 	mosquitto_tr.start()
 	tunslip6_tr = threading.Thread(target=run_tunslip)
@@ -91,7 +104,6 @@ def get_nodelist():
 		for y in webURL.readlines():
 			if "node_ipv6" in y:
 				nodeip=nodeip+y.rstrip()+"\n"
-
 		accessfile.acquire()
 		f=open("NODELIST.dat","w")
 		f.write(nodeip)
@@ -102,6 +114,7 @@ def get_nodelist():
    		time.sleep(float(time_upd_nlist))
    				
 def get_alldata():
+	nodearray=[]
 	while 1:
 		print("while get all")
 		accessfile.acquire()
@@ -110,13 +123,14 @@ def get_alldata():
 		for x in line:
 			if "node_ipv6" in x:
 				ipnode=x.split("-")[0].split("]")[0].split("[")[1].rstrip()
-				print(ipnode)
-				for res in data_resources.split(","):
-					print(res)
-					coap_handler(ipnode, "get", res, "")
-					
+				nodearray.append(ipnode)
    		f.close
    		accessfile.release()
+   		for ip in nodearray:
+   			print(ip)
+   			for res in data_resources.split(","):
+   				coap_handler(ip, "get", res, "")
+   		del nodearray[:]
 		time.sleep(float(time_between_reqall))
 
 def run_tunslip():
@@ -140,28 +154,34 @@ def run_tunslip():
     	#print("-->mid: " + str(mid))
     	
 def coap_handler(coap_address, method, resource, data):
+	#print("000000000000",threading.currentThread().getName())
 	mutex.acquire()
+	#print("111111111111",threading.currentThread().getName())
 	#usando coap-client de linux
 	ts = time.time()
 	timestamp = datetime.datetime.fromtimestamp(ts).strftime("%d-%m-%Y (%H:%M:%S.%f)")	
 	if method is 'get':
-		out = subprocess.Popen(['coap-client', '-v', '0','-B',coap_max_wtime,'-m',method,'coap://['+coap_address+']/'+resource], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)		
+		out = subprocess.Popen(['coap-client', '-v', '0','-B',coap_max_wtime,'-m',method,'coap://['+coap_address+']/'+resource], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		#print("2222222222222222",threading.currentThread().getName())		
 	if method is 'post' or method is 'put':
 		out = subprocess.Popen(['coap-client', '-v', '0','-B',coap_max_wtime,'-m',method,'-e',data,'coap://['+coap_address+']/'+resource], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)		
 	stdout,stderr = out.communicate()
 	datatosend=re.sub('v:1 t:CON.*]\s','',stdout)
 	print(datatosend.rstrip())
-	client.publish("data", "["+timestamp+"]"+"["+coap_address+"]"+"["+resource+"]"+"{"+datatosend.rstrip()+"}")
-	mutex.release()
+	#print("333333333333",threading.currentThread().getName())
+	pub.publish(idbeehive+"/data", "["+timestamp+"]"+"["+coap_address+"]"+"["+resource+"]"+"{"+datatosend.rstrip()+"}")
+	#print("444444444444",threading.currentThread().getName())
 	#usando la api de python
 	#coapc = HelperClient(server=(coap_server, 5683))
 	#cresponse = coapc.get(resource)
 	#print(cresponse.payload)
 	#coapc.stop()
- 
+	mutex.release()
+	#print("5555555555",threading.currentThread().getName())
+ 	
 def on_message(client, userdata, message):
 #mosquitto_pub -h beesmart.cure.edu.uy -p 2000 -t "send/METHOD" -u pfc -P pfc -m "ADDRESS%TOPIC%DATA"
-	#print("message received " ,str(message.payload.decode("utf-8")))
+	print("message received " ,str(message.payload.decode("utf-8")))
 	#print("message topic=",message.topic)
 	#print("message qos=",message.qos)
 	#print("message retain flag=",message.retain)
@@ -171,18 +191,55 @@ def on_message(client, userdata, message):
 		coap_handler(message.payload.split("%")[0], "post", message.payload.split("%")[1],message.payload.split("%")[2])
 	elif "put" in message.topic:
 		coap_handler(message.payload.split("%")[0], "put", message.payload.split("%")[1],message.payload.split("%")[2])
+	elif "rpicommands" in message.topic:
+		if "nodelist" in message.payload:
+			mutex.acquire()
+			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idbeehive+"/rpicommands -u "+ user+ " -P "+ passw + " -f NODELIST.dat")
+			mutex.release()
+		elif "configfile" in message.payload:
+			mutex.acquire()
+			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idbeehive+"/rpicommands -u "+ user+ " -P "+ passw + " -f BEESMART.conf")
+			mutex.release()
+		elif "iprpi" in message.payload:
+			mutex.acquire()
+			os.system("ifconfig > iptemp.temp")
+			time.sleep(0.2)
+			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idbeehive+"/rpicommands -u "+ user+ " -P "+ passw + " -f iptemp.temp")
+			time.sleep(0.2)
+			os.system("rm -rf iptemp.temp")
+			mutex.release()
+		elif "description" in message.payload:
+			mutex.acquire()
+			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idbeehive+"/rpicommands -u "+ user+ " -P "+ passw + " -m "+descbeehive)
+			mutex.release()
+		elif "rebootnodes" in message.payload:
+			mutex.acquire()
+			os.system("echo rebootnodes")
+			mutex.release()
+		elif "rebootrpi" in message.payload:
+			mutex.acquire()
+			os.system("reboot now")
+			mutex.release()
+		elif "rebootall" in message.payload:
+			mutex.acquire()
+			os.system("echo rebootall")
+			mutex.release()
 		
+			
+	
 def mosquitto_init():
-	client.username_pw_set(user, passw)
+	pub.username_pw_set(user, passw)
+	sub.username_pw_set(user, passw)
 	#client.on_subscribe = on_subscribe
 	#client.on_unsubscribe = on_unsubscribe
-	client.on_message = on_message
+	sub.on_message = on_message
 	#client.on_publish = on_publish
 	#client.on_connect = on_connect
 	#client.on_disconnect = on_disconnect
-	client.connect(broker, port)
-	client.subscribe("send/#", qos=1)	
-	client.loop_forever()
+	sub.connect(broker, port)
+	pub.connect(broker, port)
+	sub.subscribe(idbeehive+"/send/#", qos=1)	
+	sub.loop_forever()
 
 	
 if __name__== "__main__":
