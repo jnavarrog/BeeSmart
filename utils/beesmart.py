e867930a0ca1ae085c8ca24043d6d327331c77ad
jnavarrog <j@mail.com>
2020-02-22 02:37:48 -0300

    Queda "pronto" la conexion con el modem 3g y el sistema de chequeo y recuperacion de conexión del modem 3g

--- utils/beesmart.py
+++ utils/beesmart.py
@@ -36,6 +36,8 @@ def main():
 	global descapiario
 	global enable_log
 	global connect3g
+	global check3gcon
+	global tcheck3gcon
 	f=open("BEESMART.conf","r")
 	line=f.readlines()
 	#load BEESMART.conf configuration parameters
@@ -82,8 +84,14 @@ def main():
 		elif "D_USE_3GMODEM" in x:
 			connect3g=x.split("=")[1]
 			connect3g=connect3g.rstrip()
+		elif "D_ENABLE_CHK3GCON" in x:
+			check3gcon=x.split("=")[1]
+			check3gcon=check3gcon.rstrip()
+		elif "T_CHK3GCON" in x:
+			tcheck3gcon=x.split("=")[1]
+			tcheck3gcon=tcheck3gcon.rstrip()
 			
-	LOG("Configuración cargada")
+	LOG("main --> configuración cargada")
 	f.close()
 	
 	if "yes" in connect3g:
@@ -92,20 +100,25 @@ def main():
 	f=open("NODELIST.dat","w")
 	f.close()
 	
-	LOG("Se lanza thread mosquitto_tr")
+	if "yes" in check3gcon and "yes" in connect3g:
+		LOG("main --> se lanza thread chk3gcon_tr")
+		chk3gcon_tr = threading.Thread(target=chk3gcon)
+		chk3gcon_tr.start()
+	
+	LOG("main --> se lanza thread mosquitto_tr")
 	mosquitto_tr = threading.Thread(target=mosquitto_init, name='mosquitto_tr')
 	mosquitto_tr.start()
-	LOG("Se lanza thread tunslip6")
+	LOG("main --> se lanza thread tunslip6")
 	tunslip6_tr = threading.Thread(target=run_tunslip)
 	tunslip6_tr.start()
 	time.sleep(5)
-	LOG("Se lanza thread get_nodelist_tr")
+	LOG("main --> se lanza thread get_nodelist_tr")
 	get_nodelist_tr = threading.Thread(target=get_nodelist)
 	get_nodelist_tr.start()
-	LOG("Se lanza thread get_alldata_tr")
+	LOG("main --> se lanza thread get_alldata_tr")
 	get_alldata_tr = threading.Thread(target=get_alldata)
 	get_alldata_tr.start()
-	LOG("Fin main")
+	LOG("main --> fin main")
 	
 def get_nodelist():
 	nodeip=""
@@ -128,7 +141,7 @@ def get_nodelist():
    		accessfile.release()
    		nodeip=""
    		print(nodeip)
-   		LOG("Se actualiza la lista de nodos")
+   		LOG("get_nodelist --> lista de nodos actualizada")
    		time.sleep(float(time_upd_nlist))
    				
 def get_alldata():
@@ -144,55 +157,37 @@ def get_alldata():
    		f.close
    		accessfile.release()
    		for ip in nodearray:
-   			print(ip)
+   			LOG("get_alldata --> recolectado datos de: "+ip)
    			for res in data_resources.split(","):
    				coap_handler(ip, "get", res, "")
    		del nodearray[:]
-   		LOG("Se recolectaron y publicar automaticamente todos los datos")
+   		LOG("get_alldata --> iteracion finalizada")
 		time.sleep(float(time_between_reqall))
 
 def run_tunslip():
 	out = subprocess.Popen(['./tunslip6', '-s',tun_usbport,ipv6_tunnel], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	stdout,stderr = out.communicate()
 	#print(stdout)
-	
-#def on_connect(client, userdata, flags, rc):
-	LOG("Connected With Result Code: {}".format(rc))
-
-#def on_disconnect(client, userdata, rc):
-	LOG("Client Got Disconnected")
-	
-#def on_subscribe(client, userdata, mid, granted_qos):
-	#print("Client Suscribed")
-
-#def on_unsubscribe(client, userdata, mid):
-	#print("Client UnSuscribed")
-	
-#def on_publish(mqttc, obj, mid):
-    	#print("-->mid: " + str(mid))
-    	
+	 	
 def coap_handler(coap_address, method, resource, data):
 	mutex.acquire()
 	#usando coap-client de linux
 	ts = time.time()
 	timestamp = datetime.datetime.fromtimestamp(ts).strftime("%d-%m-%Y (%H:%M:%S.%f)")
-	LOG("INICIO: coap-client "+coap_address+" "+method+" "+resource+" "+data)	
+	LOG("coap_handler --> coap-client "+coap_address+" "+method+" "+resource+" "+data)	
 	if method is 'get':
 		out = subprocess.Popen(['coap-client', '-v', '0','-B',coap_max_wtime,'-m',method,'coap://['+coap_address+']/'+resource], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	if method is 'post' or method is 'put':
 		out = subprocess.Popen(['coap-client', '-v', '0','-B',coap_max_wtime,'-m',method,'-e',data,'coap://['+coap_address+']/'+resource], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)		
 	stdout,stderr = out.communicate()
-	LOG("FIN: coap-client "+coap_address+" "+method+" "+resource+" "+data)	
 	datatosend=re.sub('v:1 t:CON.*]\s','',stdout)
-	print(datatosend.rstrip())
-	LOG("Publicando datos")
-	pub.publish(idapiario+"/data", "["+timestamp+"]"+"["+coap_address+"]"+"["+resource+"]"+"{"+datatosend.rstrip()+"}")
-	LOG("Datos publicados")
+	LOG("coap_handler --> mosquitto_pub "+ datatosend.rstrip())	
+	os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idapiario+"/data -u "+ user+ " -P "+ passw + " -m \"["+timestamp+"]["+coap_address+"]["+resource+"]{"+datatosend.rstrip()+"}\"")
 	mutex.release()
+	
  	
 def on_message(client, userdata, message):
-	LOG("on message")
-	#print("message received " ,str(message.payload.decode("utf-8")))
+	LOG("on message --> mensaje recibido " + str(message.payload.decode("utf-8")))
 	#print("message topic=",message.topic)
 	#print("message qos=",message.qos)
 	#print("message retain flag=",message.retain)
@@ -205,17 +200,17 @@ def on_message(client, userdata, message):
 	elif "rpicommands" in message.topic:
 		if "nodelist" in message.payload:
 			mutex.acquire()
-			LOG("Se ejecutó el comando nodelist")
+			LOG("rpicommands --> se ejecuto el comando nodelist")
 			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idapiario+"/rpicommands -u "+ user+ " -P "+ passw + " -f NODELIST.dat")
 			mutex.release()
 		elif "configfile" in message.payload:
 			mutex.acquire()
-			LOG("Se ejecutó el comando configfile")
+			LOG("rpicommands --> se ejecuto el comando configfile")
 			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idapiario+"/rpicommands -u "+ user+ " -P "+ passw + " -f BEESMART.conf")
 			mutex.release()
 		elif "iprpi" in message.payload:
 			mutex.acquire()
-			LOG("Se ejecutó el comando iprpi")
+			LOG("rpicommands --> se ejecuto el comando iprpi")
 			os.system("ifconfig > iptemp.temp")
 			time.sleep(0.2)
 			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idapiario+"/rpicommands -u "+ user+ " -P "+ passw + " -f iptemp.temp")
@@ -224,43 +219,85 @@ def on_message(client, userdata, message):
 			mutex.release()
 		elif "description" in message.payload:
 			mutex.acquire()
-			LOG("Se ejecutó el comando description")
+			LOG("rpicommands --> se ejecuto el comando description")
 			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idapiario+"/rpicommands -u "+ user+ " -P "+ passw + " -m "+descapiario)
 			mutex.release()
 		elif "rebootrpi" in message.payload:
 			mutex.acquire()
-			LOG("Se ejecutó el comando rebootrpi")
+			LOG("rpicommands --> se ejecuto el comando rebootrpi")
 			os.system("reboot now")
 			mutex.release()
 		else:
 			mutex.acquire()
-			LOG("Se ejecutó un comando invalido")
-			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idapiario+"/rpicommands -u "+ user+ " -P "+ passw + " -m comando invalido")
+			LOG("rpicommands --> se ejecuto un comando invalido")
+			os.system("mosquitto_pub -p " + port + " -h " +  broker + " -t "+idapiario+"/rpicommands -u "+ user+ " -P "+ passw + " -m \"comando invalido\"")
 			mutex.release()
+			
+			
+def on_connect(client, userdata, flags, rc):
+	sub.subscribe(idapiario+"/send/#", qos=1)
+	LOG("mqtt on_connect --> conectado")
+
+def on_disconnect(client, userdata, rc):
+	LOG("mqtt on_disconnect --> desconectado")
+	
+def on_subscribe(client, userdata, mid, granted_qos):
+	LOG("mqtt on_suscribe --> suscripto")
+
+def on_unsubscribe(client, userdata, mid):
+	LOG("mqtt on_unsubscribe --> no suscripto")
+	
+#def on_publish(mqttc, obj, mid):
+    	#print("-->mid: " + str(mid))
 			
 	
 def mosquitto_init():
-	pub.username_pw_set(user, passw)
+	#pub.username_pw_set(user, passw)	
+	#pub.on_publish = on_publish
+	#pub.on_connect = on_connect
+	#pub.on_disconnect = on_disconnect
+	#pub.connect(broker, port)
+	sub.on_connect = on_connect
+	sub.on_disconnect = on_disconnect
 	sub.username_pw_set(user, passw)
-	#client.on_subscribe = on_subscribe
-	#client.on_unsubscribe = on_unsubscribe
+	sub.on_subscribe = on_subscribe
+	sub.on_unsubscribe = on_unsubscribe
 	sub.on_message = on_message
-	#client.on_publish = on_publish
-	#client.on_connect = on_connect
-	#client.on_disconnect = on_disconnect
 	sub.connect(broker, port)
-	pub.connect(broker, port)
-	sub.subscribe(idapiario+"/send/#", qos=1)
+	#sub.subscribe(idapiario+"/send/#", qos=1)
 	sub.loop_forever()
 	
 def modem3g(action):
+	LOG("modem3g --> "+action)
 	out = subprocess.Popen(['sakis3g',action], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         stdout,stderr = out.communicate()
 	#print(stdout)
 	if "MF190 connected to Antel" in stdout:
-		LOG("Conectado a red 3g")
+		LOG("modem3g --> conectado a red 3g")
 	if "Disconnected." in stdout or "Not connected." in stdout:
-		LOG("Desconectado de red 3g")
+		LOG("modem3g --> desconectado de red 3g")
+		
+def chk3gcon():
+	maxncon=0
+	while 1:
+		out = subprocess.Popen(['ping','-q','-c','3','beesmart.cure.edu.uy'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		stdout,stderr = out.communicate()
+		#print(stdout)
+		if "3 received" in stdout:
+			maxncon=0
+			LOG("chk3gcon --> hay conexion "+str(maxncon))
+
+		else:
+		        maxncon=maxncon+1
+			LOG("chk3gcon --> NO hay conexion "+ str(maxncon))
+		if maxncon == 5:
+			LOG("chk3gcon --> Reiniciando conexion")
+			modem3g("disconnect")
+			time.sleep(3)
+			modem3g("connect")
+			LOG("chk3gcon --> Conexion reiniciada")
+			maxncon=3
+		time.sleep(float(tcheck3gcon))
 	
 def LOG(msg):
 	if "yes" in enable_log:
